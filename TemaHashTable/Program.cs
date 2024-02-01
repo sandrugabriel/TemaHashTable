8ec5218bdd0f837db53a0197e6996bd4e7a4e733
sandrugabriel <sandru11.gabriel@gmail.com>
2024-02-01 16:10:00 +0200

    done

--- TemaHashTable/Program.cs
+++ TemaHashTable/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Linq;
 using TemaHashTable.HashTable;
 using TemaHashTable.HashTable.interfaces;
@@ -27,6 +28,30 @@ internal class Program
         return true;
     }
 
+    public static int primaLiteraNeRpetabila(IHashTable<int, char> hashTable, string text)
+    {
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            int nr = text[i];
+            hashTable.Put(nr, text[i]);
+        }
+
+        for (int i = 97; i < 256; i++)
+        {
+
+            List<char> values = hashTable.GetValues(i);
+            if (values.Count == 1)
+            {
+               // Console.WriteLine(values[0]);
+
+                return i;
+            }
+        }
+
+        return -1;    
+    }
+
     static int GetAnagramKey(string word)
     {
         char[] chars = word.ToCharArray();
@@ -40,16 +65,17 @@ internal class Program
         IHashTable<int,char> hashTable = new HashTable<int,char>(100);
         hashTable.ToString();
 
+
         //1. Unique Characters: Determine if a string has all unique characters without using additional data structures.
-        Console.WriteLine("Problema 1:\n");
+        Console.WriteLine("Problema 1:");
         string text = "asdfag";
         Console.WriteLine(caractereUnice(hashTable,text));
 
+
         //2. Group Anagrams: Given an array of strings, group anagrams together.
-        Console.WriteLine("\n\nProblema 2:\n");
+        Console.WriteLine("\n\nProblema 2:");
         string[] words = { "listen", "silent", "enlist", "eat", "tea", "ate" };
         IHashTable<int, string> anagramGroups = new HashTable<int, string>(100);
-
         foreach (string word in words)
         {
             int key = GetAnagramKey(word);
@@ -58,10 +84,8 @@ internal class Program
                 anagramGroups.Put(key, word);
             }
         }
-
         Console.WriteLine("Grupurile de anagrame:");
-
-        for (int i = 0; i < 256; i++)
+        for (int i = 97; i < 256; i++)
         {
             List<string> anagrams = anagramGroups.GetValues(i);
             if (anagrams.Count > 1)
@@ -70,6 +94,12 @@ internal class Program
             }
         }
 
+
+        //3. First Non-Repeating Character: Find the first non-repeating character in a string and return its index.If it doesn't exist, return -1.
+        Console.WriteLine("\n\nProblema 3:");
+        IHashTable<int, char> hashTable1 = new HashTable<int, char>(100);
+        string text1 = "abab";
+        Console.WriteLine("Pozitia primei litere care apare doar odata este "+primaLiteraNeRpetabila(hashTable1,text1));
 
 
 
